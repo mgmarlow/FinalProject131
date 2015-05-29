f50df77bcf9461414a66c4c063ed903d43415b57
Graham Marlow <mgmarlow@gmail.com>
2015-05-29 09:03:43 -0700

    add credit

--- src/data.R
+++ src/data.R
@@ -1,3 +1,20 @@
+# Credit to the UCI Machine Learning Repository for this data set
+#
+# set: 1985 Auto Imports Database
+# Source Information:
+# -- Creator/Donor: Jeffrey C. Schlimmer (Jeffrey.Schlimmer@a.gp.cs.cmu.edu)
+# -- Date: 19 May 1987
+# -- Sources:
+#   1) 1985 Model Import Car and Truck Specifications, 1985 Ward's
+#         Automotive Yearbook.
+#   2) Personal Auto Manuals, Insurance Services Office, 160 Water
+#         Street, New York, NY 10038 
+#   3) Insurance Collision Report, Insurance Institute for Highway
+#         Safety, Watergate 600, Washington, DC 20037
+#
+
+
+# Read data, assign variable names
 auto.data <- read.table("https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data",
                    header=F, sep=",")
 
@@ -29,5 +46,3 @@ colnames(auto.data) = c(
   'highway-mpg',
   'price'
   )
-
-head(auto.data)
--- src/project.R
+++ src/project.R
@@ -1,2 +1,9 @@
-setwd("C:/Users/Graham/Desktop/FinalProject131/src")
-source("data.R", local=TRUE)
+# Graham Marlow
+# PSTAT 131 Final Project
+# 5/29/2015
+#
+
+# load data from 'data.R'
+setwd('C:/Users/Graham/Desktop/FinalProject131/src')
+source('data.R', local=TRUE)
+head(auto.data)
