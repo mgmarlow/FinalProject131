0a34c182ae7f3251496b4300d9aa0be557d88a9b
Graham Marlow <mgmarlow@gmail.com>
2015-05-29 08:49:54 -0700

    separate project and data

---
+++ src/data.R
@@ -0,0 +1,33 @@
+auto.data <- read.table("https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data",
+                   header=F, sep=",")
+
+colnames(auto.data) = c(
+  'symboling',
+  'normalized-losses',
+  'make',
+  'fuel-type',
+  'aspiration',
+  'num-doors',
+  'body-style',
+  'drive-wheels',
+  'engine-location',
+  'wheel-base',
+  'length',
+  'width',
+  'height',
+  'curb-weight',
+  'engine-type',
+  'num-of-cylinders',
+  'engine-size',
+  'fuel-system',
+  'bore',
+  'stroke',
+  'compression-ratio',
+  'horsepower',
+  'peak-rpm',
+  'city-mpg',
+  'highway-mpg',
+  'price'
+  )
+
+head(auto.data)
