9c8a99b81bf8196cc2f7b487c4f35ffb76027867
Graham Marlow <mgmarlow@gmail.com>
2015-05-29 09:16:09 -0700

    partition test and training sets

--- src/project.R
+++ src/project.R
@@ -7,3 +7,21 @@
 setwd('C:/Users/Graham/Desktop/FinalProject131/src')
 source('data.R', local=TRUE)
 head(auto.data)
+dim(auto.data)
+
+
+# Divide data into test set and training set
+set.seed(2)
+
+# Training set, sample 75% of the data
+index <- sample(dim(auto.data)[1], size=floor(dim(auto.data)[1])*.75,
+                                              replace=F)
+train.set <- auto.data[index,]
+dim(train.set)
+
+# Test set, sample remaining observations
+index.test <- setdiff(1:dim(auto.data)[1], index)
+test.set <- auto.data[index.test,]
+dim(test.set)
+
+
