7c33745f7a453a9843151513ce610504b7070c6c
Graham Marlow <mgmarlow@gmail.com>
2015-06-07 20:45:09 -0700

    add model eval

--- src/tree.R
+++ src/tree.R
@@ -12,6 +12,9 @@ dim(new.data)
 # Attach binary variable to dataset
 tree.data <- data.frame(new.data, biSym)
 
+
+# Simple Classification Tree, no Model Evaluation
+##################################################
 # Generate tree using defaults
 car.tree <- tree(biSym ~., data=tree.data)
 summary(car.tree)
@@ -24,3 +27,23 @@ title('Classification Tree')
 
 # Model Validation
 ###################
+
+set.seed(2)
+
+# Use training set and test set to evaluate our classification tree
+car.tree <- tree(biSym ~., data=tree.data, subset=index)
+
+# Predict on test set
+tree.pred <- predict(car.tree, train.set, type='class')
+
+# Confusion matrix
+error <- table(tree.pred, test.set)
+
+
+# K-fold Cross Validation
+##########################
+
+
+
+# Classification Tree using Random Forest
+##########################################
